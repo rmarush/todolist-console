9154541b734a23ffd5a38916032442a483434ac1
rmarush <rmarushkevych@gmail.com>
2023-10-24 12:32:38 +0300

    Update .gitignore and create project

---
+++ todolist-console/todolist-console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace todolist_console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
